2dad1039ec0dd83ef9b928a5754b826657044c27
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-03-14 21:09:00 +0300

    repl

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AkifhanIlgaz/pokedex
+
+go 1.20
---
+++ repl.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func startPokedex() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+		commandName := words[0]
+		allCommands := getCommands()
+
+		command, ok := allCommands[commandName]
+		if ok {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+	}
+}
+
+func cleanInput(input string) []string {
+	input = strings.ToLower(input)
+	return strings.Fields(input)
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
